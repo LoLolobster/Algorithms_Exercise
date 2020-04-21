ee657a931d83241c71dcd06bd3b47b1520fe34db
LOLOlobster <94967574@qq.com>
2020-04-22 01:34:30 +0800

    中序转后序及中序转前序的修改

--- src/Ch01/CalcExpression.java
+++ src/Ch01/CalcExpression.java
@@ -3,6 +3,8 @@
  */
 package Ch01;
 
+import java.util.Queue;
+
 /**
  * @author HP
  * create time: 2020年4月3日下午9:38:53
@@ -140,10 +142,7 @@ public class CalcExpression {
 						output = operations.pop() + " " + output;
 						if(!operations.isEmpty()) {top = operations.peek();}
 						//栈为空时入栈
-						else {
-							operations.push(next);
-							top = next;
-						}
+						else	break;
 					}
 					//不低于栈顶优先级时，入栈
 					operations.push(next);
@@ -156,6 +155,59 @@ public class CalcExpression {
 		return output;
 	}
 	
+	/**
+	 * 中序转后序（调度场算法，和中序转前序相反）
+	 * @param infixExp 正确的中序表达式
+	 */
+	public static void InfixToPostfix(String infixExp) {
+		NodeStack<Character> operations = new NodeStack<Character>();
+		NodeQueue<Object> output = new NodeQueue<Object>();
+		String tempNum = "";
+		char next;
+		int length = infixExp.length();
+		int i = 0;
+		while(i < length) {
+			next = infixExp.charAt(i);		
+			if(Character.isDigit(next)) {
+				while(Character.isDigit(next) || next == '.') {
+					tempNum += next;
+					i++;
+					if(i < length) {next = infixExp.charAt(i);}
+				}
+				i--;
+				output.enqueue(tempNum);
+				tempNum = "";
+			}		
+			else if(next == '(') {
+				operations.push(next);
+			}	
+			else if(next == ')') {
+				char top;
+				while((top = operations.pop()) != '(') {
+					output.enqueue(top);
+				}
+			}
+			else {
+				if(operations.isEmpty()) {operations.push(next);}
+				else {
+					char top = operations.peek();
+					//输入的优先级小于等于栈顶的优先级
+					while(("+-".indexOf(next)!=-1 && "+-*/".indexOf(top)!=-1) 
+							|| ("*/".indexOf(next)!=-1 && "*/".indexOf(top)!=-1)
+							) 
+					{
+						output.enqueue(operations.pop());
+						if(operations.isEmpty()) {break;}
+						else {top = operations.peek();}
+					}
+					operations.push(next);
+				}
+			}
+			i++;
+		}
+		for(char op : operations) {output.enqueue(op);}
+		for(Object o : output) {System.out.print(o + " ");}
+	}
 	
 	/**
 	 * @param args
@@ -163,8 +215,10 @@ public class CalcExpression {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String prefix = InfixToPrefix("2*3/(2-1)+3*(4-1)");
+		System.out.println(prefix);
 		double answer =  calcPrefix(prefix);
 		System.out.println(answer);
+		InfixToPostfix("2*3/(2-1)+3*(4-1)");
 	}
 
 }
