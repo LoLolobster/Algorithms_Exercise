fbefda388cff0d7c7340368a6094564428f85832
LOLOlobster <94967574@qq.com>
2020-04-23 21:31:37 +0800

    删除作者编写的IO有关的内容

--- src/Ch01/Rational.java
+++ src/Ch01/Rational.java
@@ -173,51 +173,51 @@ public class Rational implements Comparable<Rational> {
 
     // test client
     public static void main(String[] args) {
-        Rational x, y, z;
-
-        // 1/2 + 1/3 = 5/6
-        x = new Rational(1, 2);
-        y = new Rational(1, 3);
-        z = x.plus(y);
-        StdOut.println(z);
-
-        // 8/9 + 1/9 = 1
-        x = new Rational(8, 9);
-        y = new Rational(1, 9);
-        z = x.plus(y);
-        StdOut.println(z);
-
-        // 1/200000000 + 1/300000000 = 1/120000000
-        x = new Rational(1, 200000000);
-        y = new Rational(1, 300000000);
-        z = x.plus(y);
-        StdOut.println(z);
-
-        // 1073741789/20 + 1073741789/30 = 1073741789/12
-        x = new Rational(1073741789, 20);
-        y = new Rational(1073741789, 30);
-        z = x.plus(y);
-        StdOut.println(z);
-
-        //  4/17 * 17/4 = 1
-        x = new Rational(4, 17);
-        y = new Rational(17, 4);
-        z = x.times(y);
-        StdOut.println(z);
-
-        // 3037141/3247033 * 3037547/3246599 = 841/961 
-        x = new Rational(3037141, 3247033);
-        y = new Rational(3037547, 3246599);
-        z = x.times(y);
-        StdOut.println(z);
-
-        // 1/6 - -4/-8 = -1/3
-        x = new Rational(1, 6);
-        y = new Rational(-4, -8);
-        z = x.minus(y);
-        StdOut.println(z);
-        
-        x = new Rational(-2,2);
+//        Rational x, y, z;
+//
+//        // 1/2 + 1/3 = 5/6
+//        x = new Rational(1, 2);
+//        y = new Rational(1, 3);
+//        z = x.plus(y);
+//        StdOut.println(z);
+//
+//        // 8/9 + 1/9 = 1
+//        x = new Rational(8, 9);
+//        y = new Rational(1, 9);
+//        z = x.plus(y);
+//        StdOut.println(z);
+//
+//        // 1/200000000 + 1/300000000 = 1/120000000
+//        x = new Rational(1, 200000000);
+//        y = new Rational(1, 300000000);
+//        z = x.plus(y);
+//        StdOut.println(z);
+//
+//        // 1073741789/20 + 1073741789/30 = 1073741789/12
+//        x = new Rational(1073741789, 20);
+//        y = new Rational(1073741789, 30);
+//        z = x.plus(y);
+//        StdOut.println(z);
+//
+//        //  4/17 * 17/4 = 1
+//        x = new Rational(4, 17);
+//        y = new Rational(17, 4);
+//        z = x.times(y);
+//        StdOut.println(z);
+//
+//        // 3037141/3247033 * 3037547/3246599 = 841/961 
+//        x = new Rational(3037141, 3247033);
+//        y = new Rational(3037547, 3246599);
+//        z = x.times(y);
+//        StdOut.println(z);
+//
+//        // 1/6 - -4/-8 = -1/3
+//        x = new Rational(1, 6);
+//        y = new Rational(-4, -8);
+//        z = x.minus(y);
+//        StdOut.println(z);
+//        
+//        x = new Rational(-2,2);
     }
 
 }
